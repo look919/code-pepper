cfaf48674e50a5f3d2d87b273e01bb75377c40d8
Tomasz Wirkus <wirkus919@gmail.com>
2021-12-07 22:51:34 +0100

    Initial project cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  container: {
+    maxWidth: 600
+  }
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.container}>
+      <Button variant='contained' color='primary'>
+        Start Battle
+      </Button>
     </div>
   );
 }
